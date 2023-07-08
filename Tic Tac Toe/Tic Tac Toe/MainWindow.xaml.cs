4cb8370e9b306023c070b821324950603c322d6f
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-07-09 00:45:22 +0400

    Game Finished
    Base game is finished but it still has some changes to do. game is playable and you can restart game.

--- Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace Tic_Tac_Toe
 {
@@ -19,16 +20,21 @@ namespace Tic_Tac_Toe
     {
         private readonly Dictionary<Player, ImageSource> imageSources = new()
         {
-            { Player.X, new BitmapImage(new Uri("pack://application:,,,Assets/X15.png")) },
-            { Player.O, new BitmapImage(new Uri("pack://application:,,,Assets/O15.png")) }
+            { Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            { Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
         };
 
         private readonly Image[,] imageControls = new Image[3, 3];
         private readonly GameState gameState = new GameState();
+
         public MainWindow()
         {
             InitializeComponent();
             SetUpGameGrid();
+
+            gameState.MoveMade += OnMoveMade;
+            gameState.GameEnded += onGameEnded;
+            gameState.GameRestarted += onGameRestarted;
         }
 
         private void SetUpGameGrid()
@@ -43,14 +49,70 @@ namespace Tic_Tac_Toe
                 }
             }
         }
+
+        private void TransitionToEndScreen(string text, ImageSource winnerImage)
+        {
+            TurnPanel.Visibility = Visibility.Hidden;
+            GameCanvas.Visibility = Visibility.Hidden;
+            ResultText.Text = text;
+            WinnerImage.Source = winnerImage;
+            EndScreen.Visibility = Visibility.Visible;
+        }
+
+        private void TransitionToGameScreen()
+        {
+            EndScreen.Visibility = Visibility.Hidden;
+            TurnPanel.Visibility = Visibility.Visible;
+            GameCanvas.Visibility = Visibility.Visible;
+        }
+
+        private void OnMoveMade(int r, int c)
+        {
+            Player player = gameState.GameGrid[r, c];
+            imageControls[r, c].Source = imageSources[player];
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+        }
+
+        private async void onGameEnded(GameResult gameResult)
+        {
+            await Task.Delay(1000);
+
+            if (gameResult.Winner == Player.None)
+            {
+                TransitionToEndScreen("It is a tie!", null);
+            }
+            else
+            {
+                TransitionToEndScreen("Winner: ", imageSources[gameResult.Winner]);
+            }
+        }
+
+        private void onGameRestarted()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].Source = null;
+                }
+            }
+
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+            TransitionToGameScreen();
+        }
+
         private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            double squareSize = GameGrid.Width / 3;
+            Point clickPosition = e.GetPosition(GameGrid);
+            int row = (int)(clickPosition.Y / squareSize);
+            int col = (int)(clickPosition.X / squareSize);
+            gameState.MakeMove(row, col);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            gameState.Reset();
         }
     }
 }
