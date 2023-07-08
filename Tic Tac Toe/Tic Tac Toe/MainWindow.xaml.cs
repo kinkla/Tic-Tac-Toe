b71edfc5be187b338db53904bc2048dae009f86f
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-07-08 23:28:50 +0400

    Added logics
    Added color resources, wrote UI and GameGrid setup

--- Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/Tic Tac Toe/MainWindow.xaml.cs
@@ -15,14 +15,42 @@ using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<Player, ImageSource> imageSources = new()
+        {
+            { Player.X, new BitmapImage(new Uri("pack://application:,,,Assets/X15.png")) },
+            { Player.O, new BitmapImage(new Uri("pack://application:,,,Assets/O15.png")) }
+        };
+
+        private readonly Image[,] imageControls = new Image[3, 3];
+        private readonly GameState gameState = new GameState();
         public MainWindow()
         {
             InitializeComponent();
+            SetUpGameGrid();
+        }
+
+        private void SetUpGameGrid()
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 3; c++)
+                {
+                    Image imageControl = new Image();
+                    GameGrid.Children.Add(imageControl);
+                    imageControls[r, c] = imageControl; 
+                }
+            }
+        }
+        private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
